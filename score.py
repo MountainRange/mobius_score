00670f448789b4c9defb0c2f785d343a37fb361f
rkeezer3 <rmkeezer@yahoo.com>
2017-10-14 11:51:45 -0400

    Added spectrograph and beats

--- score.py
+++ score.py
@@ -10,19 +10,53 @@ def scoreSong(args):
     score(args[0])
 
 def score(songname):
-    song, sample_rate = librosa.load("moonlight.mp3", sr=44100)
+    song, sample_rate = librosa.load("moonlightshort.mp3", sr=44100)
 
-    mel = librosa.feature.melspectrogram(song, sr=sample_rate, n_mels=128)
+    # D = np.abs(librosa.stft(song))**2
 
-    log_mel = librosa.power_to_db(mel, ref=np.max)
+    # mel = librosa.feature.melspectrogram(S=D)
+    # log_mel = librosa.power_to_db(mel, ref=np.max)
+
+    cqt = librosa.cqt(song, sr=sample_rate, fmin=librosa.note_to_hz('C1'),
+                        n_bins=84*4, bins_per_octave=12*4, hop_length=64, filter_scale=0.2)
+    amp_cqt = librosa.amplitude_to_db(cqt, ref=np.max)
+
+    print(cqt.size)
+    print(amp_cqt.size)
+    print(cqt[0].size)
+    print(cqt[0])
+    print(amp_cqt[0].size)
+    # s = int(amp_cqt[0].size/2)
+    # print(s)
+    # print(amp_cqt[:,s])
 
     plt.figure(figsize=(12,4))
 
-    librosa.display.specshow(log_mel, sr=sample_rate, x_axis='time', y_axis='mel')
+    tempo, beats = librosa.beat.beat_track(y=song, sr=sample_rate, hop_length=64)
+    tempo2, quarterBeats = librosa.beat.beat_track(y=song, sr=sample_rate, hop_length=64, bpm=tempo*4)
 
-    plt.colorbar(format='%+02.0f dB')
+    print(beats)
+    print(quarterBeats)
+    print(tempo)
 
-    plt.show()
+    #librosa.display.specshow(amp_cqt, sr=sample_rate, x_axis='time', y_axis='hz')
+
+    # plt.vlines(librosa.frames_to_time(quarterBeats)/2,
+    #         1, 0.5 * sample_rate,
+    #         colors='w', linestyles='-', linewidth=2, alpha=0.5)
+
+    # plt.colorbar(format='%+2.0f dB')
+
+    # plt.show()
+
+    avgBeat = np.median(np.diff(beats))
+    w = int(avgBeat / 2)
+    features = []
+    for q in quarterBeats:
+        features.append(amp_cqt[:,q-w:q+w])
+    print(len(features))
+
+    tensorDemo(features)
 
 def tensorTrain(train_data, train_labels, clf):
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -50,13 +84,22 @@ def tensorEval(eval_data, eval_labels, clf):
     return clf.evaluate(input_fn=eval_input_fn)
 
 
-def tensorDemo():
-    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-    train_data = mnist.train.images
-    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-    eval_data = mnist.test.images
-    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-   
+def tensorDemo(features):
+    # mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    # train_data = mnist.train.images
+    # train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    # eval_data = mnist.test.images
+    # eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+    
+    # print(train_data)
+    # print(train_data[0])
+    # print(train_data[0].size)
+    # print(train_data.size)
+
+    print(len(features))
+
+    return
+
     clf = tf.estimator.Estimator(
         model_fn=cnn, model_dir="tmp/coolModel")
 
@@ -121,5 +164,4 @@ def cnn(features, labels, mode):
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 if __name__ == "__main__":
-    #scoreSong(sys.argv)
-    tensorDemo()
+    scoreSong(sys.argv)
